ac4602a9acdf1989dccb3308a313c499eadad437
Diego M Otero <babelsolo@gmail.com>
2022-04-19 00:11:17 -0500

    Screen && Actions

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,10 +1,12 @@
-import { SafeAreaView, Text } from 'react-native'
 import React, { useState, useEffect } from 'react'
+import { SafeAreaView } from 'react-native'
 import { getPokemonsApi, getPokemonDetailsByUrlApi } from '../api/pokemon'
+import PokemonList from '../components/PokemonList'
 
 export default function Pokedex() {
 
-  const [ pokemons, setPokemos ] = useState([]) 
+  const [ pokemons, setPokemons ] = useState([])
+  const [ nextUrl, setNextUrl ]   = useState(null)
 
   useEffect(() => {
     (async () => {
@@ -14,16 +16,23 @@ export default function Pokedex() {
 
   const loadPokemons = async () => {
     try {
-      const response = await getPokemonsApi()
+      const response = await getPokemonsApi(nextUrl)
+      setNextUrl(response.next)
+
       const pokemonsArray = []
 
-      for await (const pokemon of response.result){
-        const pokemonDetail = await getPokemonDetailsByUrlApi(pokemon.url)
-        console.log(pokemon)
-        console.log(pokemonDetail)
+      for await (const pokemon of response.results){
+        const pokemonDetails = await getPokemonDetailsByUrlApi(pokemon.url)
+        pokemonsArray.push({
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+          type: pokemonDetails.types[0].type.name,
+          order: pokemonDetails.order,
+          image: pokemonDetails.sprites.other['official-artwork'].front_default
+        })
       }
 
-      setPokemos(pokemonsArray)
+      setPokemons([...pokemons, ...pokemonsArray])
     } catch (error) {
       console.log(error)
     }
@@ -31,7 +40,11 @@ export default function Pokedex() {
 
   return (
     <SafeAreaView>
-      <Text>Pokedex</Text>
+      <PokemonList 
+        pokemons={pokemons}
+        loadPokemons={loadPokemons}
+        isNext={nextUrl}
+      />
     </SafeAreaView>
   )
 }
---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+    username: "diego_m_otero",
+    password: "123456789",
+  };
+  
+  export const userDetails = {
+    username: "diego_m_otero",
+    firstName: "Diego M",
+    lastName: "Otero",
+    email: "babelsolo@gmail.com",
+  };
